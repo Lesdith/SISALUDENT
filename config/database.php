0d5c248bb2299115e1f696ceddd531130cea07f6
Lesdit <lesdith@gmail.com>
2018-11-20 18:19:36 -0600

    Se creó una nueva conexión

--- config/database.php
+++ config/database.php
@@ -31,23 +31,66 @@ return [
     |
     */
 
-    'connections' => [
+'connections' => [
 
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => env('DB_HOST', '35.226.17.143'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'mysql'),
-            'username' => env('DB_USERNAME', 'root'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => env('DB_DATABASE', 'sisaludent_db'),
+            'username' => env('DB_USERNAME', 'sisaludent_user'),
+            'password' => env('DB_PASSWORD', 'SiSaluDent1.'),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
-            'collation' => 'utf8mb4_unicode_ci',
+            'collation' => 'utf8mb4_spanish_ci',
             'prefix' => '',
             'strict' => true,
             'engine' => null,
         ],
+
+
+// 'connections' => [
+
+
+//         'mysql' => [
+//             'driver' => 'mysql',
+//             'host' => env('DB_HOST', 'db4free.net'),
+//             'port' => env('DB_PORT', '3306'),
+//             'database' => env('DB_DATABASE', 'sisaludent_db'),
+//             'username' => env('DB_USERNAME', 'sisaludent_user'),
+//             'password' => env('DB_PASSWORD', 'SiSaluDent1.'),
+//             'unix_socket' => env('DB_SOCKET', ''),
+//             'charset' => 'utf8mb4',
+//             'collation' => 'utf8mb4_spanish_ci',
+//             'prefix' => '',
+//             'strict' => true,
+//             'engine' => null,
+//         ],
+
+
+
+    // 'connections' => [
+
+
+    //     'mysql' => [
+    //         'driver' => 'mysql',
+    //         'host' => env('DB_HOST', '127.0.0.1'),
+    //         'port' => env('DB_PORT', '3306'),
+    //         'database' => env('DB_DATABASE', 'mysql'),
+    //         'username' => env('DB_USERNAME', 'root'),
+    //         'password' => env('DB_PASSWORD', ''),
+    //         'unix_socket' => env('DB_SOCKET', ''),
+    //         'charset' => 'utf8mb4',
+    //         'collation' => 'utf8mb4_unicode_ci',
+    //         'prefix' => '',
+    //         'strict' => true,
+    //         'engine' => null,
+    //     ],
+
+
+
+
 
          'sqlite' => [
             'driver' => 'sqlite',
