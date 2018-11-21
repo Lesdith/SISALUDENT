b92d1117749b3a944120de591688f6ec7f502b8a
Lesdit <lesdith@gmail.com>
2018-11-21 14:00:19 -0600

    Holi

--- config/database.php
+++ config/database.php
@@ -34,20 +34,20 @@ return [
 'connections' => [
 
 
-    //     'mysql' => [
-    //         'driver' => 'mysql',
-    //         'host' => env('DB_HOST', '127.0.0.1'),
-    //         'port' => env('DB_PORT', '3306'),
-    //         'database' => env('DB_DATABASE', 'mysql'),
-    //         'username' => env('DB_USERNAME', 'root'),
-    //         'password' => env('DB_PASSWORD', ''),
-    //         'unix_socket' => env('DB_SOCKET', ''),
-    //         'charset' => 'utf8mb4',
-    //         'collation' => 'utf8mb4_unicode_ci',
-    //         'prefix' => '',
-    //         'strict' => true,
-    //         'engine' => null,
-    //     ],
+        'mysql' => [
+            'driver' => 'mysql',
+            'host' => env('DB_HOST', '127.0.0.1'),
+            'port' => env('DB_PORT', '3306'),
+            'database' => env('DB_DATABASE', 'mysql'),
+            'username' => env('DB_USERNAME', 'root'),
+            'password' => env('DB_PASSWORD', ''),
+            'unix_socket' => env('DB_SOCKET', ''),
+            'charset' => 'utf8mb4',
+            'collation' => 'utf8mb4_unicode_ci',
+            'prefix' => '',
+            'strict' => true,
+            'engine' => null,
+        ],
 
 
 
