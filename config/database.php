5cd69bc94c53830ad4302f37e68ebfdaea2a0d72
lesdith <lesdith@gmail.com>
2018-12-03 22:29:51 -0600

    Se  realizaron modificaciones en USerHasRole

--- config/database.php
+++ config/database.php
@@ -43,7 +43,7 @@ return [
             'password' => env('DB_PASSWORD', ''),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
-            'collation' => 'utf8mb4_unicode_ci',
+            'collation' => 'utf8mb4_spanish_ci',
             'prefix' => '',
             'strict' => true,
             'engine' => null,
